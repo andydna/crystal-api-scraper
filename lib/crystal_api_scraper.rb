417aec3e22ac08651458f8a2c79913a939a36f80
andydna <andydna@protonmail.ch>
2020-05-08 05:45:04 -0500

    refactoring

--- lib/crystal_api_scraper.rb
+++ lib/crystal_api_scraper.rb
@@ -4,50 +4,81 @@ require 'open-uri'
 CrystalMethod = Struct.new(:signature, :summary, keyword_init: true)
 
 class CrystalApiScraper
+  XPATH = { 'MethodList' => '//ul[@class="list-summary"]',
+            'Method'     => './li[@class="entry-summary"]',
+            'Signature'  => './a',
+            'Summary'    => './div' }
+
   def initialize(klass)
     @klass = klass
-  end
-
-  def html
-    @html ||= URI.open(url).read
+    @scopes = Hash.new {|hsh, key| hsh[key] = Array.new}
   end
 
   def constructors
-    crystal_methods['Constructors']
+    @constructors ||= summaries['Constructors'];
   end
 
   def class_methods
-    crystal_methods['Class Methods']
+    @class_methods ||= summaries['Class Methods']
   end
 
   def instance_methods
-    crystal_methods['Instance Methods']
+    @instance_methods ||= summaries['Instance Methods']
   end
 
   private
 
-  def url
-    "https://crystal-lang.org/api/master/#{@klass}.html"
-  end
-
-  def crystal_methods
-    list_summaries.each.with_object({}) do |list_summary, lists|
-      scope = list_summary.previous_element.text.strip.sub(' Summary', 's')
+  def summaries
+    list_summaries.each.with_object({}) do |list, lists|
+      scope = trim_h2(list.previous_element.text)
       lists[scope] = Array.new unless lists[scope]
 
-      list_summary.xpath('./li[@class="entry-summary"]').each do |method_summary|
-        lists[scope] << CrystalMethod.new(
-         signature: method_summary.xpath('./a').inner_text,
-         summary:   method_summary.xpath('./div').inner_text)
-      end
+      list.xpath(XPATH['Method']).each.with_object(scope, &extract_crystal_method)
     end
+    @scopes
   end
 
   def list_summaries
-    noko.xpath('//ul[@class="list-summary"]')
+    noko.xpath(XPATH['MethodList'])
+  end
+
+  def extract_crystal_method
+    Proc.new do |method_summary, scope|
+      @scopes[scope] << CrystalMethod.new(
+       signature: method_summary.xpath(XPATH['Signature']).inner_text,
+       summary:   method_summary.xpath(XPATH['Summary']).inner_text)
+    end
+  end
+
+  def trim_h2(text)
+    text.strip.sub(' Summary', 's')
   end
 
   def noko
     Nokogiri.HTML(html)
+  end
+
+  def html
+    cached? ? cache : URI.open(url, &read_io)
+  end
+
+  def cached?
+    File.exists? cache_path
+  end
+
+  def cache
+    File.open(cache_path, "r", &read_io)
+  end
+
+  def read_io
+    Proc.new { |io| next io.read }
+  end
+
+  def cache_path
+    "cache/#{@klass}.html"
+  end
+
+  def url
+    "https://crystal-lang.org/api/master/#{@klass}.html"
   end
 end
--- spec/crystal_api_scraper_spec.rb
+++ spec/crystal_api_scraper_spec.rb
@@ -1,6 +1,8 @@
-require 'crystal_api_scraper'
-
 require 'vcr'
+require 'pry'
+require 'fileutils'
+
+require 'crystal_api_scraper'
 
 VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr"
@@ -9,25 +11,56 @@ VCR.configure do |config|
 end
 
 RSpec.describe CrystalApiScraper, :vcr do
-  let(:scraper) { CrystalApiScraper.new('Class') }
+  context 'summaries' do
+    let(:scraper) { CrystalApiScraper.new('Class') }
 
-  it 'given a class, it scrapes the html' do
-    html = scraper.html
-    expect(html).to include "Returns the name of this class."
+    it 'generates a list of constructors' do
+      constructors = scraper.constructors
+      expect(constructors.count).to eq 1
+    end
+
+    it 'generates a list of class methods' do
+      class_methods = scraper.class_methods
+      expect(class_methods.count).to eq 14
+    end
+
+    it 'generates a list of instance methods' do
+      instance_methods = scraper.instance_methods
+      expect(instance_methods.count).to eq 15
+    end
+
+    it 'spotcheck constructor: cast' do
+      cast = scraper.constructors.find do |constructor|
+        constructor.signature == ".cast(other) : self"
+      end
+      expect(cast).to be_an_instance_of(CrystalMethod)
+      expect(cast).to include("Casts other to this class.")
+    end
+
+    it 'spotcheck class method: name' do
+      name = scraper.class_methods.find do |class_method|
+        class_method.signature == ".name : String"
+      end
+      expect(name).to be_an_instance_of(CrystalMethod)
+      expect(name).to include("Returns the name of this class.")
+    end
+
+    it 'spotcheck instance method: dup' do
+      dup = scraper.instance_methods.find do |instance_method|
+        instance_method.signature == "#dup"
+      end
+      expect(dup).to be_an_instance_of(CrystalMethod)
+      expect(dup).to include("Returns a shallow copy of this object.")
+    end
   end
 
-  it 'generates a list of constructors' do
-    constructors = scraper.constructors
-    expect(constructors.count).to eq 1
-  end
+  it "doesn't scrape if we've already cached it" do
+    FileUtils.mkdir('cache') unless File.exists?('cache')
+    FileUtils.touch('cache/Klass.html')
 
-  it 'generates a list of class methods' do
-    class_methods = scraper.class_methods
-    expect(class_methods.count).to eq 14
-  end
+    uri = class_double('URI').as_stubbed_const
 
-  it 'generates a list of instance methods' do
-    instance_methods = scraper.instance_methods
-    expect(instance_methods.count).to eq 15
+    expect(uri).not_to receive(:open)
+    CrystalApiScraper.new('Klass').constructors
   end
 end
